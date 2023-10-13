20d2f7fdba37321ecfde001e57ce2e6fb01552db
Ibrahim Okić <ibrahimokic711@gmail.com>
2023-10-13 21:38:59 +0200

    CreateTransaction method

--- interfaces/Transaction.interface.hpp
+++ interfaces/Transaction.interface.hpp
@@ -2,6 +2,15 @@
 #define TRANSACTION_INTERFACE_H
 
 #include <fstream>
+#include <vector>
+
+struct ITransaction
+{
+    string type;
+    double amount;
+    string from;
+    string to;
+};
 
 class Transaction
 {
@@ -12,25 +21,53 @@ private:
     string To;
 
 public:
-    Transaction(string type, double amount, string from, string to)
+    Transaction(ITransaction props = {})
     {
-        this->Type = type;
-        this->Amount = amount;
-        this->From = from;
-        this->To = to;
+        this->Type = props.type;
+        this->Amount = props.amount;
+        this->From = props.from;
+        this->To = props.to;
     }
 
     void Serialize(std::ostream &outFile) const;
     void Deserialize(std::istream &inFile);
 
-    void CreateTransaction()
+    static inline void CreateTransaction(ITransaction *props)
     {
-        std::ofstream database("database/data/transactions.dat", std::ios::binary | std::ios::out | std::ios::trunc);
+        std::vector<Transaction> transactions;
+        std::fstream database("database/data/transactions.dat", std::ios::binary | std::ios::in | std::ios::out);
 
+        // >> If there is no file then create it
         if (!database.is_open())
         {
             std::cerr << "ERROR: Nije moguce uspostaviti konekciju sa databazom." << std::endl;
-            return;
+            std::fstream database("database/data/transactions.dat", std::ios::binary | std::ios::in | std::ios::out | std::ios::trunc);
+        }
+
+        // >> Read all transactions from file and push them in vector 'transactions'
+        Transaction transaction;
+        while (true)
+        {
+            transaction.Deserialize(database);
+
+            if (!database)
+            {
+                break;
+            }
+
+            transactions.push_back(transaction);
+        }
+
+        Transaction newTransaction(*props);
+        transactions.push_back(newTransaction);
+
+        // >> Reopen database but this time in write mode
+        database.close();
+        database.open("database/data/transactions.dat", std::ios::binary | std::ios::out | std::ios::trunc);
+
+        for (const auto &transaction : transactions)
+        {
+            transaction.Serialize(database);
         }
 
         database.close();
@@ -42,7 +79,7 @@ void Transaction::Serialize(std::ostream &outFile) const
     outFile << Type << '|'
             << Amount << '|'
             << From << '|'
-            << To << '|' << "|\n";
+            << To << "|\n";
 }
 
 void Transaction::Deserialize(std::istream &inFile)
@@ -51,6 +88,13 @@ void Transaction::Deserialize(std::istream &inFile)
 
     std::string amount;
     std::getline(inFile, amount, '|');
+    try
+    {
+        Amount = std::stod(amount);
+    }
+    catch (const std::invalid_argument &e)
+    {
+    }
 
     std::getline(inFile, From, '|');
     std::getline(inFile, To, '|');
